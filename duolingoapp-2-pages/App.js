b6dbf03a01fc71a3be875df3f60c7ebb7a5f59f0
Bruno Ricardo Wotzke <49734747+BrunoRW@users.noreply.github.com>
2023-04-26 12:32:08 -0300

    Add files via upload

---
+++ duolingoapp-2-pages/App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Profile from "./pages/profile";
+
+import Dash from "./pages/dash";
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
